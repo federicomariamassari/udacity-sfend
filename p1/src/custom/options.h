965a95b696243f8cbcd2305f56d85cb24f0b4b26
Federico Massari <federico.massari@bocconialumni.it>
2023-08-18 00:19:27 +0200

    code: Update options to p1 code

--- p1/src/custom/options.h
+++ p1/src/custom/options.h
@@ -1,6 +1,8 @@
 #ifndef OPTIONS_H
 #define OPTIONS_H
 
+/* Options to customize Udacity's Sensor Fusion LiDAR Obstacle Detection project.
+ */
 struct Options
 {
   // Rendering options
@@ -42,10 +44,7 @@ struct Options
       renderPlaneCloud(true),  // true to render inliers in green
       renderObstacleCloud(true),  // true to render obstacles (non-inliers) in red
 
-      renderClusters(true),  // true to render Euclidean Clustering on obstacle data
-
-      renderBoxes(true),  // true to render bounding boxes around obstacle data
-      renderMinimumXyAlignedBoxes(false)  // true to render XY-plane-aligned minimum bounding boxes
+      renderClusters(true)  // true to render Euclidean Clustering on obstacle data
   {
     if (renderCityBlock)
     {  
@@ -56,20 +55,33 @@ struct Options
         // ----------------------------------------------------------------------------------------
         filepath = "../src/sensors/data/pcd/data_2";
 
-        // TODO: Fine-tune the parameters and use minimum bounding box!
-        clusterTolerance = 0.4;
-        minSize = 11;  // To track the guard rails
-        maxSize = 950;
+        renderBoxes = true;  // true to render bounding boxes around obstacle data
+        renderMinimumXyAlignedBoxes = false;
+
+        // RANSAC 3D
+        maxIterations = 60;
+        distanceThreshold = 0.27;  // 27 cm
+
+        clusterTolerance = 0.32;  // 32 cm
+        minSize = 9;  // To track the guard rails
+        maxSize = 1200;
       }
-      
+
       else
       {
         filepath = "../src/sensors/data/pcd/data_1";
 
+        renderBoxes = false;  // true to render bounding boxes around obstacle data
+        renderMinimumXyAlignedBoxes = true;  // true to render XY-plane-aligned minimum bounding boxes
+
+        // RANSAC 3D
+        maxIterations = 50;
+        distanceThreshold = 0.15;  // 15 cm
+
         // Euclidean clustering
         clusterTolerance = 0.45;  // 45 cm
-        minSize = 19;
-        maxSize = 600;
+        minSize = 15;  // To track the side pole
+        maxSize = 800;
       }
 
       // ------------------------------------------------------------------------------------------
@@ -80,16 +92,12 @@ struct Options
 
       // Voxel grid
       filterPointCloud = true;  // true to downsample point cloud using a voxel grid filter
-      filterRes = 0.2;  // Downsample point cloud with cubic voxels of side 20 cm
-      
-      // Region of interest (box; meters):
-      // X: 30N, 10S; Y: 5E, 6W; Z in [-2; 1] since LiDAR is mounted on top of egoCar
+      filterRes = 0.20;  // Downsample point cloud with cubic voxels of side 20 cm
+
+      // Region of interest - a box with the dimensions (meters): X in [30 North; 10 South],
+      // Y in [5 East; 6 West]; Z in [-2 down; 1 up] since LiDAR is mounted on top of egoCar
       minPoint = {-10.0, -5.0, -2.0, 1.0};
       maxPoint = {30.0, 6.0, 1.0, 1.0};
-
-      // RANSAC 3D
-      maxIterations = 50;
-      distanceThreshold = 0.15;  // 15 cm
     }
 
     else
